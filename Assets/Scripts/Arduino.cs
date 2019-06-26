9d0272c0b2925b0467dc418a78cf244ddcb0c65b
067-francisco-s@sapo.corppt.com <fsfregalado@ua.pt>
2019-06-26 13:04:11 +0100

    Add connectivity with touchboard

---
+++ Assets/Scripts/Arduino.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO.Ports;
+public class Arduino : MonoBehaviour
+{
+    SerialPort ard;
+    public string com = "COM1"; public int baudrate = 9600; public int sentido = 1;
+    void Start()
+    {
+        ard = new SerialPort(com, baudrate); ard.Open();
+        ard.ReadTimeout = 1000;
+    }
+    void Update()
+    {
+        if (ard.IsOpen)
+        {
+            try
+            {
+                string pot = ard.ReadLine();
+                print(pot);
+            }
+            catch (System.Exception)
+            {
+                throw;
+            }
+        }
+    }
+    public void Close()
+    {
+        ard.Close();
+    }
+}
